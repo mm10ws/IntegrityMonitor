95aa71bddaa3c44e72039c2b07e64a0032ddf6e6
mm <mm@example.com>
2016-04-13 23:12:10 -0400

    added directory scanner

---
+++ src/Scanner.java
@@ -0,0 +1,72 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class Scanner {
+    public String root = "";
+    public ArrayList<String> listOfFiles = new ArrayList<String>();
+    public ArrayList<String> listOfDirs = new ArrayList<String>();
+
+    public Scanner(String root) {
+        this.root = root;
+    }
+
+    public static void main(String args[]) {
+        Scanner s = new Scanner(""); //set root directory here
+
+        try {
+            s.getFiles(s.getRoot());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Files:");
+        for (int i = 0; i < s.listOfFiles.size(); i++) {
+            System.out.print(s.listOfFiles.get(i) + " ");
+        }
+
+        System.out.println();
+        System.out.println("Dirs:");
+        for (int i = 0; i < s.listOfDirs.size(); i++) {
+            System.out.print(s.listOfDirs.get(i) + " ");
+        }
+    }
+
+    public String getRoot() {
+        return root;
+    }
+
+    public void setRoot(String root) {
+        this.root = root;
+    }
+
+    public ArrayList<String> getListOfFiles() {
+        return listOfFiles;
+    }
+
+    public void setListOfFiles(ArrayList<String> listOfFiles) {
+        this.listOfFiles = listOfFiles;
+    }
+
+    public ArrayList<String> getListOfDirs() {
+        return listOfDirs;
+    }
+
+    public void setListOfDirs(ArrayList<String> listOfDirs) {
+        this.listOfDirs = listOfDirs;
+    }
+
+    public void getFiles(String path) throws Exception {
+        File currentDir = new File(path);
+        File[] all = currentDir.listFiles();
+
+        for (int i = 0; i < all.length; i++) {
+            if (all[i].isFile()) {
+                this.listOfFiles.add(all[i].getName());
+            } else if (all[i].isDirectory()) {
+                this.listOfDirs.add(all[i].getName());
+            } else {
+                //not a file or directory
+            }
+        }
+    }
+}
